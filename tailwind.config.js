f7553d55d92e7bbd8e3bbc63ed51fd582a707ab2
DeveloperKareemElsharkawy <dev.kareem.elsharkawy@gmail.com>
2023-05-16 18:22:43 +0200

    New Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -120,6 +120,5 @@ module.exports = {
     plugins: [
       require('@salla.sa/twilight-tailwind-theme'),
       require('@tailwindcss/forms'),
-      require('@tailwindcss/line-clamp'),
-    ],
+     ],
 }
